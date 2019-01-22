987301c2df884a5a71eccf57cdda1f6415ee7852
Peter Pham <phamhere@gmail.com>
2019-01-21 19:46:19 -0700

    implemented wordcount in views.py and count.html

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 
 def home(request):
@@ -7,4 +8,14 @@ def home(request):
 
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    worddict = {}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+    sortedwords = sorted(
+        worddict.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sortedwords': sortedwords})
